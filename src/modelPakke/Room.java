564d13cace756eee9a44cab90d2b7ba5cdde7c1f
Asbjørn Steinskog <asbjornsteinskog@gmail.com>
2013-03-11 13:55:42 +0100

    roomTaken

--- src/modelPakke/Room.java
+++ src/modelPakke/Room.java
@@ -2,12 +2,15 @@ package modelPakke;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Room {
 
 	private int size;
 	private String name;
 	private PropertyChangeSupport propChangeSupp;
+	private ArrayList<Date> roomTaken;
 	
 	// Constructor
 	public Room(int size, String name) {
@@ -37,6 +40,14 @@ public class Room {
 		propChangeSupp.firePropertyChange(event);
 	}
 	
+	//roomTaken
+	public void addTaken(Date date) {
+		this.roomTaken.add(date);
+	}
+	
+	public void removeTaken(Date date) {
+		this.roomTaken.remove(this.roomTaken.indexOf(date));
+	}
 	
 	/*
 	 * Her må vi ha en egen kalender som kan holde oversikt over når rommet er ledig 
