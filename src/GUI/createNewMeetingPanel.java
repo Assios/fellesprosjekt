224bef36dd17dee8b652e5e0e85f668cdaaba261
Håkon <hakonmaa@gmail.com>
2013-03-12 15:37:41 +0100

    La til liste og noen felt

--- src/GUI/createNewMeetingPanel.java
+++ src/GUI/createNewMeetingPanel.java
@@ -1,7 +1,50 @@
 package GUI;
 
 import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JList;
 
 public class createNewMeetingPanel extends JFrame{
-
+	private JTextField textField;
+	public createNewMeetingPanel() {
+		
+		JLabel lblMeetingName = new JLabel("Meeting name:");
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JLabel lblMemberList = new JLabel("Member list:");
+		
+		JList list = new JList();
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(list, GroupLayout.DEFAULT_SIZE, 115, Short.MAX_VALUE)
+						.addComponent(lblMeetingName)
+						.addComponent(lblMemberList)
+						.addComponent(textField))
+					.addContainerGap(688, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblMeetingName)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblMemberList)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(list, GroupLayout.PREFERRED_SIZE, 257, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(142, Short.MAX_VALUE))
+		);
+		getContentPane().setLayout(groupLayout);
+	}
 }
