7efe2efab331bdea7938b0cc6f7b1e1257286028
pettek2003 <pettek2003@gmail.com>
2013-03-20 17:10:15 +0100

    Update calendarLogic.java

--- src/modelPakke/calendarLogic.java
+++ src/modelPakke/calendarLogic.java
@@ -1,5 +1,6 @@
 package modelPakke;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,70 +18,91 @@ import java.util.ArrayList;
 import java.util.Vector;
 import DBconnection.Events;
 
+
 import javax.swing.JComboBox;
 import javax.swing.ListModel;
+
 
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 
+
 public class calendarLogic {
 	XStream xs = new XStream();
-	
+
+
 	Socket serverConnection;
     ObjectOutputStream out;
     ObjectInputStream in;
     String fromServer;
     String disconnect="dc";
     ArrayList<Meeting> meetings;
+    ArrayList<User> availableUsers;
 
-	
+
+
 	public calendarLogic() {
 	}
 
-	
+
+
+
 	public String send(Object object){
 		try{
-		
+
+
 			try{
-				
+
+
 				Gson gson = new Gson();
 				User user = new User("sfsdf","John Doe","124124", "john@gmail.com", new Date(1992, 23, 05));
 				String json = gson.toJson(user);
 				out.writeObject(json);
 	    			out.flush();
-	    	
+
+
 	    			fromServer=(String)in.readObject();
 	    			return(fromServer);
-		
-	    		
+
+
+
+
 	    	}
 	    	catch(IOException ioException){
 	    		return("Something went wrong with i/o");
 	    	}
-			
+
+
 		} catch (Exception e) {
 			// TODO: handle exception
 			return("no connection");
 		}
-		
-		
+
+
+
+
 	}
 
-	
+
+
+
 	public void disConnect(){
 		try {
-			
+
+
 			serverConnection.close();
 			System.out.println("disconnected");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
+
 	}
 
+
 	public void connect() {
-		try{
+		try{//78.91.48.59
     		serverConnection = new Socket("78.91.16.62", 7899);
     		System.out.println("Connected to  78.91.16.62  in port 7899");
     		//2. get Input and Output streams
@@ -96,58 +118,80 @@ public class calendarLogic {
 
 
 
+
+
+
 	}
-	
+
+
+
 
 	public String requestLogin(String id, String password){
 		Request r=new Request("login",id,password,null);
-		
+
+
 		try{			
 			try{	
+					Response resp;
 					out.writeObject(r);
-	    			fromServer=(String)in.readObject();
-	    			return(fromServer);
-	    		
+					out.flush();
+	    			resp=(Response)in.readObject();
+	    			availableUsers=new ArrayList<User>(resp.getUsers());
+	    			return(resp.getResult());
+
+
 	    	}
 	    	catch(IOException ioException){
 	    		return("Something went wrong with i/o");
 	    	}
-			
+
+
 		} catch (Exception e) {
 			// TODO: handle exception
 			return("no connection");
 		}
-		
+
+
 	}
-	
+
+
 	public void createCalendarEvent(String avtaleNavn, String leader, Time start, Time slutt, Date dato, Vector<String> invited, String description){
 		String oE="opprettEvent";
-		
+
+
 		Request r=new Request(oE,avtaleNavn,leader,start,slutt,dato,invited,description);
 		try{			
 			try{	
 					out.writeObject(r);
 					System.out.println("ddfgdf");
 	    			out.flush();
-	    			
-	    		
+
+
+
+
 	    	}
 	    	catch(IOException ioException){
-	    		
+
+
 	    	}
-			
+
+
 		} catch (Exception e) {
 			// TODO: handle exception
-		
+
+
 		}
 	}
-	
-	public ArrayList<Events> getEvents(String user){
+
+
+	public ArrayList<Meeting> getEvents(String user){
 		Request r=new Request("hentEvents",user);
 		try {
 			out.writeObject(r);
+			
 			out.flush();
-			return (ArrayList)in.readObject();
+			return (ArrayList<Meeting>)in.readObject();
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -159,19 +203,24 @@ public class calendarLogic {
 		return null;
 	}
 	
-	public void updateMeetings(String user){
-		ArrayList<Events> events=getEvents(user);
-		
-		for(int i=0;i<events.size();i++){
-			Meeting m=new Meeting(events.get(i).getAvtaleID(),events.get(i).getTid(),events.get(i).getAvtaleNavn(),events.get(i).getBeskrivelse(),events.get(i).getSted(),events.get(i).getIsActive(), user);
-			meetings.add(m);
-			
-		}
-		
-		
-		
+	public ArrayList<User> getAvailableMembers(){
+		return availableUsers;
 	}
-	
+
+	public void updateMeetings(String user){
+		meetings=getEvents(user);
+
+
+		
+
+
+
+
+
+
+	}
+
+
 	public JComboBox addDatesToJComboBox(JComboBox cb){
 		int day,hour;
 		Date dato;
@@ -183,7 +232,8 @@ public class calendarLogic {
 		}
 		return cb;
 	}
-	
+
+
 	public JComboBox addHoursToJComboBox(JComboBox cb){
 		int hour=0;
 		for(int i=0;i<24;i++){
@@ -193,11 +243,12 @@ public class calendarLogic {
 		}
 		return cb;
 	}
-	
+
+
 	public String convertToJson(Object o){
 		//return(xs.toJson(o));
 		return("");
 	}
 
-}
 
+}
