bb735160458692c42e4ce0df16ce8f4708f0a116
Asbjørn Ottesen Steinskog <asbjorn@steinskog.me>
2013-03-20 11:52:35 +0100

    Adds Request file.

---
+++ src/modelPakke/Request.java
@@ -0,0 +1,92 @@
+package modelPakke;
+
+
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Date;
+import java.util.Vector;
+
+
+public class Request implements java.io.Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	String userName,password,avtaleNavn, leader,type,description;
+	Time start, slutt;
+	Date dato;
+	Vector<String> invited;
+	Object object;
+	
+	public Request(String t,String uN, String password, Object o) {
+		this.type=t;
+		this.userName=uN;
+		this.password=password;
+		object=o;
+	}
+
+
+	public Request(String t,String uN){
+		type=t;
+		this.userName=uN;
+	}
+
+
+	public Request(String t,String avtaleNavn, String leader, Time start, Time slutt, Date dato, Vector<String> invited, String description){
+		this.type=t;
+		this.avtaleNavn=avtaleNavn;
+		this.leader=leader;
+		this.start=start;
+		this.slutt=slutt;
+		this.dato=dato;
+		this.invited=invited;
+		this.description=description;
+	}
+
+
+	public String getDescription(){
+		return description;
+	}
+
+	public Vector<String> getInvited(){
+		return invited;
+	}
+
+
+	public Date getDate(){
+		return dato;
+	}
+
+
+	public Time getEndTime(){
+		return slutt;
+	}
+
+
+	public Time getStartTime(){
+		return start;
+	}
+
+
+	public String getLeader(){
+		return leader;
+	}
+
+
+	public String getAvtaleNavn(){
+		return avtaleNavn;
+	}
+
+
+	public String getType(){
+		return type;
+	}
+
+
+	public String getUserName(){
+		return userName;
+	}
+
+
+	public String getPassword(){
+		return password;
+	}
+}
