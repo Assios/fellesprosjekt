e593ad0d9c97a1d491eee26c9551bf56e682532d
pettek2003 <pettek2003@gmail.com>
2013-03-21 11:27:46 +0100

    Update calendarLogic.java

--- src/modelPakke/calendarLogic.java
+++ src/modelPakke/calendarLogic.java
@@ -127,3 +127,160 @@ public class calendarLogic {
 
 	}
 
+
+
+
+	public String requestLogin(String id, String password){
+		Request r=new Request("login",id,password,null);
+		Response resp;
+
+		try{			
+			try{	
+					
+					user.setUserName(id);
+					out.writeObject(r);
+					out.reset();
+					out.flush();
+	    			resp=(Response)in.readObject();
+	    			availableUsers=new ArrayList<User>(resp.getUsers());
+	    			return(resp.getResult());
+
+
+	    	}
+	    	catch(IOException ioException){
+	    		return("Something went wrong with i/o");
+	    	}
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return("no connection");
+		}
+
+
+	}
+
+
+	public void createCalendarEvent(String avtaleNavn, String leader, Time start, Time slutt, Date dato, String description){
+		String oE="opprettEvent";
+		
+
+		Request r=new Request(oE,avtaleNavn,leader,start,slutt,dato,invited,description);
+		try{			
+			try{	
+					out.writeObject(r);
+					System.out.println("ddfgdf");
+	    			out.flush();
+	    	}
+	    	catch(IOException ioException){
+
+
+	    	}
+
+
+		} catch (Exception e) {
+			// TODO: handle exception
+
+
+		}
+	}
+
+	public ArrayList<Room> getRooms(){
+		return rooms;
+	}
+	
+	public void updateRooms(){
+		Request r=new Request("hentRom","");
+		try {
+			out.writeObject(r);
+			
+			out.flush();
+			rooms=(ArrayList<Room>)in.readObject();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("no arraylist could be retrieved");
+	}
+	
+	public boolean timeOverlaps(TimeInterval t1, TimeInterval t2){
+		if(t1.getStart().getTime()>=t2.getSlutt().getTime() || t1.getSlutt().getTime()<=t2.getStart().getTime())
+			return false;
+		else
+			return true;
+	}
+	
+	public User getUser(){
+		return user;
+	}
+
+	public ArrayList<Meeting> getEvents(){
+		
+		return meetings;
+	}
+	
+	public ArrayList<User> getAvailableMembers(){
+		return availableUsers;
+	}
+
+	public void updateMeetings(){
+		System.out.println(user.getUserName()+" hher");
+		Request r=new Request("hentEvents",user.getUserName());
+		try {
+			out.writeObject(r);
+			
+			out.flush();
+			meetings=(ArrayList<Meeting>)in.readObject();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("no arraylist could be retrieved");
+
+	}
+
+
+	public JComboBox addDatesToJComboBox(JComboBox cb){
+		int day,hour;
+		Date dato;
+		for(int i=0;i<12;i++){
+			for(int j=0;j<30;j++){
+				dato=new Date(2013,i,j);
+				cb.addItem(dato);
+			}
+		}
+		return cb;
+	}
+
+
+	public JComboBox addHoursToJComboBox(JComboBox cb){
+		int hour=0;
+		for(int i=0;i<24;i++){
+			Time t=new Time(hour,00,00);
+			cb.addItem(t);
+			hour++;
+		}
+		return cb;
+	}
+	
+	public void makeInvites(ArrayList<User> users){
+		for(int i=0;i<users.size();i++){
+			invited.add(users.get(i).getUserName());
+		}
+	}
+
+	public String convertToJson(Object o){
+		//return(xs.toJson(o));
+		return("");
+	}
+
+
+}
