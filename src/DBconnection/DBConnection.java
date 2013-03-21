09c9048249f8a7dea9a7c70edabfadaf83eacd1f
pettek2003 <pettek2003@gmail.com>
2013-03-21 14:16:51 +0100

    Update DBConnection.java

--- src/DBconnection/DBConnection.java
+++ src/DBconnection/DBConnection.java
@@ -1,17 +1,20 @@
 package DBconnection;
 
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.* ;
 import java.util.Properties;
 
+
 public class DBConnection {
 	private Properties properties; 	// file containing the connection properties
 	private String jdbcDriver; 		// String containing the driver Class name
 	private String url; 			// Address to the database
 	private Connection conn;
-		
+
+
 	public DBConnection(String propertiesFilename) throws IOException,
 	ClassNotFoundException, SQLException {
 		File f = new File(propertiesFilename);
@@ -20,19 +23,23 @@ public class DBConnection {
 		jdbcDriver = properties.getProperty("jdbcDriver");
 		url = properties.getProperty("url");
 	}
-		
+
+
 	public DBConnection(Properties properties) throws ClassNotFoundException,
 	SQLException {
 		this.properties = properties;
 		jdbcDriver = properties.getProperty("jdbcDriver");
 		url = properties.getProperty("url");
-		
+
+
 	}
-		
+
+
 	public void initialize() throws ClassNotFoundException, SQLException {
 		Class.forName(jdbcDriver);
 		Properties info = new Properties();
-		
+
+
 		/* info is a different Properties object from the "properties" one.
 		 "info" is used by the Driver Manager while properties is used by our
 		program*/
@@ -40,17 +47,20 @@ public class DBConnection {
 		info.setProperty("password", this.properties.getProperty("password"));
 		conn = DriverManager.getConnection(url, info);
 	}
-		
+
+
 	public ResultSet makeSingleQuery(String sql) throws SQLException {
 		Statement st = conn.createStatement();
 		return st.executeQuery(sql);
 	}
-		
+
+
 	public void makeSingleUpdate(String sql) throws SQLException {
 		Statement st = conn.createStatement();
 		st.executeUpdate(sql);
 	}
-	
+
+
 	public ResultSet makeSingleUpdate(String sql, boolean dummy) throws SQLException {
 		Statement st = conn.createStatement();
 		st.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
@@ -60,12 +70,16 @@ public class DBConnection {
 		}
 		return null;
 	}
-		
+
+
 	public PreparedStatement preparedStatement(String sql) throws SQLException {
 		return conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
 	}
+
 
 	public void close() throws SQLException {
 		conn.close();
 	}
 }
+
+
