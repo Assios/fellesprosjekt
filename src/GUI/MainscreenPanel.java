1ca47c3b58bcf9ca5757fb18bbbb286d69067316
pettek2003 <pettek2003@gmail.com>
2013-03-21 11:21:01 +0100

    Update MainscreenPanel.java

--- src/GUI/MainscreenPanel.java
+++ src/GUI/MainscreenPanel.java
@@ -1,6 +1,9 @@
 package GUI;
 
 
+
+
+
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JFrame;
@@ -25,37 +28,55 @@ import java.awt.Dimension;
 import modelPakke.*;
 
 
+
+
 public class MainscreenPanel extends JFrame{
 	//private JTable kalender;
-	private calendarExample.Cal kalender;
+	private Cal kalender;
 	private calendarLogic cL;
-	modelPakke.Calendar calendar;
 	public MainscreenPanel(calendarLogic cl) {
-		calendar = new modelPakke.Calendar(cl.getEvents(cl.getUser().getUserName()), cl.getUser());
+		cL=cl;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setMinimumSize(new Dimension(600, 400));
+		setMinimumSize(new Dimension(600, 300));
 		setTitle("Mainframe");
 
 
-		JLabel Kallender = new JLabel("Calendar");
-		Kallender.setFont(new Font("Tahoma", Font.BOLD, 17));
-		Kallender.setHorizontalAlignment(SwingConstants.CENTER);
+
+
+		JLabel Month = new JLabel("Month");
+		Month.setFont(new Font("Tahoma", Font.BOLD, 17));
+		Month.setHorizontalAlignment(SwingConstants.CENTER);
+
+
+
+
+		JLabel Uke = new JLabel("Uke");
+		Uke.setHorizontalAlignment(SwingConstants.CENTER);
+		Uke.setFont(new Font("Tahoma", Font.BOLD, 15));
+
+
 
 
 		JLabel lblUser = new JLabel("user:");
 		lblUser.setFont(new Font("Tahoma", Font.PLAIN, 10));
 
 
-		JLabel navn = new JLabel(cL.getUser().getUserName());
+		
+
+		JLabel navn = new JLabel("username'");
 		navn.setFont(new Font("Tahoma", Font.PLAIN, 10));
+
+
 
 
 		JButton New = new JButton("New");
 		New.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				new createNewMeetingPanel(calendar).setVisible(true);
+				new createNewMeetingPanel(cL).setVisible(true);
 			}
 		});
+
+
 
 
 		JButton Edit = new JButton("Edit");
@@ -63,18 +84,41 @@ public class MainscreenPanel extends JFrame{
 		Edit.setFont(new Font("Tahoma", Font.PLAIN, 10));
 
 
-		JButton Detailes = new JButton("view");
-		Detailes.addActionListener(new ActionListener() {
+
+
+		JButton detailButton = new JButton("Details");
+		detailButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				new viewDayPanel(kalender.getActiveDate(),calendar).setVisible(true);
+				//new showMeetingInfoPanel(new modelPakke.Meeting().setVisible(true);
+				
 			}
 		});
-		Detailes.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		detailButton.setFont(new Font("Tahoma", Font.PLAIN, 10));
 
 
-		kalender = new calendarExample.Cal();
+
+
+		kalender = new Cal(cL.getEvents());
+
 		
 		kalender.setBorder(new LineBorder(new Color(0, 0, 0)));
+
+
+
+
+		JButton Left = new JButton("<<");
+
+
+
+
+		JButton Right = new JButton(">>");
+
+
+
+
+		JButton Delete = new JButton("Delete");
+		Delete.setEnabled(false);
+		Delete.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -86,37 +130,57 @@ public class MainscreenPanel extends JFrame{
 							.addComponent(navn, GroupLayout.DEFAULT_SIZE, 83, Short.MAX_VALUE))
 						.addGroup(groupLayout.createSequentialGroup()
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(Edit, GroupLayout.DEFAULT_SIZE, 171, Short.MAX_VALUE)
-								.addComponent(New, GroupLayout.DEFAULT_SIZE, 171, Short.MAX_VALUE)
-								.addComponent(Detailes, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 171, Short.MAX_VALUE))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(Delete, GroupLayout.DEFAULT_SIZE, 62, Short.MAX_VALUE)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(detailButton, GroupLayout.DEFAULT_SIZE, 63, Short.MAX_VALUE))
+								.addComponent(Edit, GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
+								.addComponent(New, GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE))
 							.addPreferredGap(ComponentPlacement.RELATED)))
+					.addGap(18)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(18)
-							.addComponent(kalender, GroupLayout.DEFAULT_SIZE, 369, Short.MAX_VALUE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(141)
-							.addComponent(Kallender, GroupLayout.PREFERRED_SIZE, 103, GroupLayout.PREFERRED_SIZE)))
+							.addComponent(Left)
+							.addGap(103)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(Month, GroupLayout.PREFERRED_SIZE, 77, GroupLayout.PREFERRED_SIZE)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(10)
+									.addComponent(Uke, GroupLayout.PREFERRED_SIZE, 53, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED, 58, Short.MAX_VALUE)
+									.addComponent(Right))))
+						.addComponent(kalender, GroupLayout.DEFAULT_SIZE, 265, Short.MAX_VALUE))
 					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
 						.addGroup(groupLayout.createSequentialGroup()
 							.addComponent(lblUser)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(navn, GroupLayout.PREFERRED_SIZE, 13, GroupLayout.PREFERRED_SIZE))
-						.addComponent(Kallender, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE))
-					.addGap(42)
+							.addComponent(navn, GroupLayout.PREFERRED_SIZE, 13, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(Right)
+							.addGap(10))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(Month, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(Uke)
+							.addGap(18))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(Left)
+							.addPreferredGap(ComponentPlacement.UNRELATED)))
 					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
 						.addGroup(groupLayout.createSequentialGroup()
 							.addComponent(New, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE)
 							.addPreferredGap(ComponentPlacement.RELATED)
 							.addComponent(Edit)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(Detailes))
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addComponent(detailButton)
+								.addComponent(Delete, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)))
 						.addComponent(kalender, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(35))
 		);
@@ -124,12 +188,11 @@ public class MainscreenPanel extends JFrame{
 	}
 
 
+
+
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-
-	}
+	   
 }
+
